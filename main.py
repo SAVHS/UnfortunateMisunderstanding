5132e5f6f4c4c50ca32e2e622039d4170ee65596
John Dough <liketoaccess@gmail.com>
2021-12-08 10:45:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from direct.showbase.ShowBase import ShowBase
+from direct.actor import Actor
+from panda3d.core import WindowProperties
 
-class Game(Showbase):
+class Game(ShowBase):
 	def __init__(self):
-		Showbase.__init__(self)
+		ShowBase.__init__(self)
+
 
 		properties = WindowProperties()
-        properties.setSize(1000, 750)
-        self.win.requestProperties(properties)
-
-        
+		properties.setSize(1000, 750)
+		self.win.requestProperties(properties)
+		self.box = loader.loadModel("models/environment")
+		self.box.reparentTo(self.render)
+		
+game = Game()
+game.run()
