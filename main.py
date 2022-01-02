30c299198b5ea478e4e08ed7e80d64df78ee36bc
John Dough <liketoaccess@gmail.com>
2022-01-02 15:16:40 -0600

    New Camera Angle + Colliders

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from panda3d.ai import *
 from direct.showbase.ShowBase import ShowBase
-from pandac.PandaModules import *
 from direct.task import Task
 from direct.actor.Actor import Actor
 from panda3d.ai import AIWorld
 from panda3d.core import WindowProperties
 from panda3d.core import Spotlight, DirectionalLight, AmbientLight
 from panda3d.core import Vec4, Vec3
-
+from panda3d.core import CollisionTraverser, CollisionHandlerPusher, CollisionSphere, CollisionTube, CollisionNode
+from panda3d.core import *
 #from GameObj import *
 
 class Game(ShowBase):
 	def __init__(self):
 		ShowBase.__init__(self)
-		self.camera.setPos(0, -20, 32)
-		self.camera.setP(-60)
+		self.camera.setPos(0, 0, 40)
+		self.camera.setP(-90)
 		self.setWindow()
 		self.setLighting()
 		self.loadWorld()
@@ -48,39 +48,56 @@ class Game(ShowBase):
 		self.tower = loader.loadModel("models/room_industrial")
 		self.tower.reparentTo(render)
 		self.tower.setScale(0.075)
-		self.tower.setPos(0, 2, 5)
+		self.tower.setPos(0, 6, .25)
 
 		self.tower2 = loader.loadModel("models/room_industrial")
 		self.tower2.reparentTo(render)
 		self.tower2.setScale(0.075)
-		self.tower2.setPos(0, -6, 5)
+		self.tower2.setPos(0, -6, 0.25)
 
 		self.tower3 = loader.loadModel("models/room_industrial")
 		self.tower3.reparentTo(render)
 		self.tower3.setScale(0.075)
-		self.tower3.setPos(4, -3.4, 5)
+		self.tower3.setPos(5, -3, 0.25)
 
 		self.tower4 = loader.loadModel("models/room_industrial")
 		self.tower4.reparentTo(render)
 		self.tower4.setScale(0.075)
-		self.tower4.setPos(-4, -3.4, 5)
+		self.tower4.setPos(-5, -3, 0.25)
 
 		self.tower5 = loader.loadModel("models/room_industrial")
 		self.tower5.reparentTo(render)
 		self.tower5.setScale(0.075)
-		self.tower5.setPos(-4, 0, 5)
+		self.tower5.setPos(-5, 3, 0.25)
 
 		self.tower6 = loader.loadModel("models/room_industrial")
 		self.tower6.reparentTo(render)
 		self.tower6.setScale(0.075)
-		self.tower6.setPos(4, 0, 5)
+		self.tower6.setPos(5, 3, 0.25)
 
 	def loadSeeker(self):
 		# Seeker
 		self.seeker = Actor("models/act_p3d_chan", {"run": "models/a_p3d_chan_run"})
 		self.seeker.reparentTo(render)
 		
-		self.seeker.setPos(Vec3(-10, 0, 0))
+		self.seeker.setPos(Vec3(0, 0, 0))
+		cNodePanda = self.seeker.attachNewNode(CollisionNode('cnode_panda'))
+		cNodePanda.node().addSolid(CollisionSphere(0, 0, 0.8, 0.8))
+		cNodePanda.show()
+		self.picker = CollisionTraverser()
+		self.picker.showCollisions(render)
+		self.pq = CollisionHandlerQueue()
+
+		self.pickerNode = CollisionNode('mouseRay')
+		self.pickerNP = camera.attachNewNode(self.pickerNode)
+		self.pickerNode.setFromCollideMask(BitMask32.bit(1))
+		self.seeker.setCollideMask(BitMask32.bit(1))
+
+		self.pickerRay = CollisionRay()
+		self.pickerNode.addSolid(self.pickerRay)
+		self.picker.addCollider(self.pickerNP, self.pq)
+
+		self.accept("mouse1", self.mouseClick)
 		# Target
 		self.target = self.tower
 		
@@ -89,7 +106,7 @@ class Game(ShowBase):
 		# Creating AI World
 		self.AIworld = AIWorld(render)
 
-		self.AIchar = AICharacter("seeker", self.seeker, 200, 1, 1)
+		self.AIchar = AICharacter("seeker", self.seeker, 100, 1, 1)
 		self.AIworld.addAiChar(self.AIchar)
 		self.AIbehaviors = self.AIchar.getAiBehaviors()
 
@@ -103,6 +120,23 @@ class Game(ShowBase):
 	def AIUpdate(self, task):
 		self.AIworld.update()
 		return Task.cont
+
+	def mouseClick(self):
+		print('mouse click')
+		# check if we have access to the mouse
+		if base.mouseWatcherNode.hasMouse():
+
+			# get the mouse position
+			mpos = base.mouseWatcherNode.getMouse()
+
+			# set the position of the ray based on the mouse position
+			self.pickerRay.setFromLens(base.camNode, mpos.getX(), mpos.getY())
+			self.picker.traverse(render)
+			# if we have hit something sort the hits so that the closest is first and highlight the node
+			if self.pq.getNumEntries() > 0:
+				self.pq.sortEntries()
+				pickedObj = self.pq.getEntry(0).getIntoNodePath()
+				print('click on ' + pickedObj.getName())
 		
 game = Game()
 game.run()
